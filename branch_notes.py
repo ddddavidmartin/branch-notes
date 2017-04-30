df7f670c08cd8ea86cb8e8a62e856763880b8c3a
David Martin <david_martin@fastmail.com>
2017-04-30 18:59:16 +1000

    Slightly rephrase help string for editor option to improve it.
    It sounds slightly better that way.

--- branch_notes.py
+++ branch_notes.py
@@ -37,9 +37,11 @@ def main():
                         help=("The project directory name under which the "
                               "notes file for the given branch is created."))
     parser.add_argument('--editor', type=str,
-                        help=("The program to create and open notes. Uses this "
-                              "variable if set, otherwise environment variable "
-                              "'%s' or finally vi." % EDITOR_VARIABLE))
+                        help=("The program used to create and open notes. If "
+                              "set, branch_notes uses the given program. "
+                              "Otherwise it tries to use the program specified "
+                              "in the environment variable '%s', or finally "
+                              "vi." % EDITOR_VARIABLE))
     options = parser.parse_args()
 
     # If CURRENT_BRANCH_OPTION is provided as the target branch we use the
